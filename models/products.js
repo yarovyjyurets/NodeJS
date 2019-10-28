83d62b22d41495842b12c62765a6f9a80cdcce75
Yurets <yurii_yarovyi@epam.com>
2019-10-28 10:30:37 +0200

    refactor readFile method

--- models/products.js
+++ models/products.js
@@ -10,10 +10,11 @@ const PRODUCTS_PATH = path.join(
 const writeFile = promisify(fs.writeFile);
 const readFile = (...args) => {
   return promisify(fs.readFile).call(null, ...args)
+    .then(rawData => JSON.parse(rawData))
     .catch(async (e) => {
       if (e.code === 'ENOENT') {
-        const defaultData = JSON.stringify([]);
-        await writeFile(PRODUCTS_PATH, defaultData);
+        const defaultData = [];
+        await writeFile(PRODUCTS_PATH, JSON.stringify(defaultData));
         return defaultData;
       } else {
         throw e;
@@ -22,22 +23,19 @@ const readFile = (...args) => {
 }
 
 const getAll = async () => {
-  const rawData = await readFile(PRODUCTS_PATH);
-  const products = JSON.parse(rawData);
+  const products = await readFile(PRODUCTS_PATH);
   return products;
 };
 
 const addProduct = async (product) => {
-  const rawData = await readFile(PRODUCTS_PATH);
-  const products = JSON.parse(rawData);
+  const products = await readFile(PRODUCTS_PATH);
   product.id = Math.ceil(Math.random() * 100).toString();
   products.push(product);
   return writeFile(PRODUCTS_PATH, JSON.stringify(products));
 };
 
 const getProductById = async (id) => {
-  const rawData = await readFile(PRODUCTS_PATH);
-  const products = JSON.parse(rawData);
+  const products = await readFile(PRODUCTS_PATH);
   const product = products.find(p => p.id === id);
 
   return product;
